26058d8fcf24865453927f5802e6a58f5c48f47e
childshc-github <henry.childs@duke.edu>
2024-04-08 15:50:50 -0400

    class

---
+++ Atomic_Ruler.py
@@ -0,0 +1,10 @@
+# formatted Lovell rotamer library originally pulled from:
+# /donaldlab/OSPREY3/blob/main/src/main/resources/edu/duke/cs/osprey/gui/conflib/lovell.conflib
+
+class Valine:
+    CB = [106.812000, 16.566000, -3.004000]
+
+
+# calculate Euclid distance
+
+# Output ranked file
